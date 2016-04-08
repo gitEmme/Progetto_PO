e162f94e6e5a2d52b91613e88523274e4149c086
xenord <francesco.benetello921@gmail.com>
2016-04-08 10:57:27 +0200

    Sistemata formattazione Aggressive urge e sistemato println che non stampava correttamente lo stack

--- CardGame/src/cardgame/cards/AggressiveUrge.java
+++ CardGame/src/cardgame/cards/AggressiveUrge.java
@@ -26,8 +26,8 @@ public class AggressiveUrge implements Card {
     
     public String toString() { return name() + "[" + rule_text() +"]";}
     
-    private class AggressiveUrgeEffect extends AbstractCardEffect implements TriggerAction
-    {
+    private class AggressiveUrgeEffect extends AbstractCardEffect implements TriggerAction {
+        
         private Player owner;/* creo la variabile giocatore per poter accedere ai metodi*/
         private Creature old,decorated;/*creo le variabili old e decorated di tipo creatura per indicare rispettivamente la vecchia e la nuova (decorata) creatura*/
         public AggressiveUrgeEffect(Player p,Card c) {
@@ -38,26 +38,31 @@ public class AggressiveUrge implements Card {
         public void resolve(){
         int nCreatures = owner.get_creatures().size();/*memorizzo il numero di creature nel campo*/
         int creatureSelected=0;
-        try
-        {    
-            /*System.out.println("nCreatures = " +nCreatures);*/
-            do
-            {    
-                System.out.println("Select one creature for which apply the effect: 0 - " + (nCreatures -1));
-                Scanner s = CardGame.instance.get_scanner();/*seleziono quale creatura applicare l'effetto impostare public get_scanner*/
-                creatureSelected = s.nextInt();
-            }while(creatureSelected<0);    
+        int i;
+
+    for(i=0;i<nCreatures;i++) {
+
+        System.out.println("Creature in the player field:" + " " + i + owner.get_creatures().get(i).name() + "Creature attack= " + " " + owner.get_creatures().get(i).get_power() + "Creature toughness= " + " " + owner.get_creatures().get(i).get_toughness());
+    }
+
+
+    try {    
+        /*System.out.println("nCreatures = " +nCreatures);*/
+        do {    
+            System.out.println("Select one creature for which apply the effect: 0 - " + (nCreatures -1));
+            Scanner s = CardGame.instance.get_scanner();/*seleziono quale creatura applicare l'effetto impostare public get_scanner*/
+            creatureSelected = s.nextInt();
+            } while(creatureSelected<0);    
         
-            old = owner.get_creatures().remove(creatureSelected);/*salvo la vecchia creatura e la rimuovo dal campo*/
-            decorated = new AggressiveUrgeDecorator(old);/*creo la nuova creatura da modificare(decorare)*/
-            owner.get_creatures().add(decorated);/*aggiungo la creatura modificata al campo*/
-            System.out.println("Creature name" + decorated + "Power and toughness: " + decorated.get_power()  + " " +  decorated.get_toughness() );
-            CardGame.instance.get_triggers().register(Phases.END_FILTER, this);/*seleziono il turno e se finisce eseguo il trigger (execute), end_filter impostato a public*/
-        }
-        catch(IndexOutOfBoundsException e)
-        {    
-            System.out.println("Non ci sono carte in campo!");
-        }  
+        old = owner.get_creatures().remove(creatureSelected);/*salvo la vecchia creatura e la rimuovo dal campo*/
+        decorated = new AggressiveUrgeDecorator(old);/*creo la nuova creatura da modificare(decorare)*/
+        owner.get_creatures().add(decorated);/*aggiungo la creatura modificata al campo*/
+        System.out.println("Creature name" + decorated + "Power and toughness: " + decorated.get_power()  + " " +  decorated.get_toughness() );
+        CardGame.instance.get_triggers().register(Phases.END_FILTER, this);/*seleziono il turno e se finisce eseguo il trigger (execute), end_filter impostato a public*/
+    }
+    catch(IndexOutOfBoundsException e) {    
+        System.out.println("Non ci sono carte in campo!");
+    }  
         
         
         } 
@@ -70,6 +75,4 @@ public class AggressiveUrge implements Card {
             }
         }
     }
-    
-    
 }
