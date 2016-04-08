8e6ecda725355e6f1c11a9b605e9067ade769ddd
albysora <galvanitos@yahoo.it>
2016-04-08 14:33:45 +0200

    interfaccia aggiornata

--- CardGame/src/cardgame/Creature.java
+++ CardGame/src/cardgame/Creature.java
@@ -7,20 +7,22 @@ public interface Creature {
     boolean tap();
     boolean untap();
     boolean isTapped();
-    void attack();
-    void defend(Creature c);
+    void attack_creature(Creature c, int dmg);
+    void attack_player(Player avversario, int dmg);
+    void defend(Creature c, int dmg);
     void inflict_damage(int dmg);
     void reset_damage();
     int get_power();
     int get_toughness();
+    int get_shield();
     
-    //metodi implementati
     public int getCurrent_power();
     public int getCurrent_toughness();
     public int getCurrent_shield();
     public void setCurrent_power(int dmg);
     public void setCurrent_toughness(int dmg);
     public void setCurrent_shield(int dmg);
+
     
     // returns all the effects associated to this creature
     List<Effect> effects();
