347207d52b48e12cebb4d3a2ce7144942d36cf4a
albysora <galvanitos@yahoo.it>
2016-04-10 01:49:53 +0200

    Aggiornata abstractcreature
    aggiornata per combatphase

--- CardGame/src/cardgame/AbstractCreature.java
+++ CardGame/src/cardgame/AbstractCreature.java
@@ -5,7 +5,7 @@ package cardgame;
 public abstract class AbstractCreature implements Creature {
     protected Player owner;
     protected boolean is_tapped=false;
-    protected int damage_left = get_toughness();
+    protected int damage_left = getCurrent_toughness();
         
         protected AbstractCreature(Player owner) { this.owner=owner; }
         
@@ -43,10 +43,19 @@ public abstract class AbstractCreature implements Creature {
         
         public void defend(Creature c, int dmg) {
             int real_dmg = 0;
-            if(dmg > this.getCurrent_shield())
+            if(dmg > this.getCurrent_shield()){
                 real_dmg = dmg - this.getCurrent_shield();
-            System.out.println(c.name() + " attaccando ha inflitto " + real_dmg + " danni al mostro " + this.name());
-            this.inflict_damage(dmg);
+                if(real_dmg <= this.getCurrent_toughness())
+                    System.out.println(c.name() + " attaccando ha inflitto " + real_dmg + " danni al mostro " + this.name());
+                else{
+                    real_dmg = this.getCurrent_toughness();
+                    System.out.println(c.name() + " attaccando ha inflitto " + real_dmg + " danni al mostro " + this.name());
+                
+                }
+            }
+            else
+                System.out.println(c.name() + " attaccando ha inflitto " + real_dmg + " danni al mostro " + this.name());
+            inflict_damage(dmg);
         } // to do in assignment 2
         
         public void inflict_damage(int dmg) {
@@ -55,6 +64,7 @@ public abstract class AbstractCreature implements Creature {
             else
                 dmg = dmg - getCurrent_shield();
                 reset_shield();
+                damage_left = getCurrent_toughness();
                 damage_left -= dmg;
                 if (damage_left<=0)
                     owner.destroy(this);       
