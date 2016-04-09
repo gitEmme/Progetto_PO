99bd1a196ef216b0ed58d0648a487d321fdbc4cf
xenord <francesco.benetello921@gmail.com>
2016-04-09 12:45:18 +0200

    Aggiunto boiling earth

--- CardGame/src/cardgame/cards/Boiling_Earth.java
+++ CardGame/src/cardgame/cards/Boiling_Earth.java
@@ -7,24 +7,48 @@
 package cardgame.cards;
 
 import cardgame.AbstractCardEffect;
-import cardgame.AbstractCardEffect;
-import cardgame.Card;
 import cardgame.Card;
 import cardgame.Effect;
-import cardgame.Effect;
 import cardgame.Player;
-import cardgame.Player;
-import cardgame.*;
+import cardgame.CardGame;
+import cardgame.Creature;
+import cardgame.Phases;
+import cardgame.TriggerAction;
 
 /**
  *
  * @author azanolla
  */
 public class Boiling_Earth implements Card{
-    private class Boiling_EarthEffect extends AbstractCardEffect {
+    private class Boiling_EarthEffect extends AbstractCardEffect implements TriggerAction  {
         public Boiling_EarthEffect (Player p, Card c) {super (p,c);}
-        public void resolve(){
+        public void resolve(){  //per ogni carta dei due player tolgo 1 punto di toughness
+            
+            
+            for (Creature c: CardGame.instance.get_current_player().get_creatures()){ 
+                c.setCurrent_toughness(c.getCurrent_toughness()-1);
+                if (c.getCurrent_toughness()==0)
+                    owner.destroy(c);
+            }    
+            for (Creature c: CardGame.instance.get_current_adversary().get_creatures()){
+                c.setCurrent_toughness(c.getCurrent_toughness()-1);
+                if (c.getCurrent_toughness()==0)
+                    owner.destroy(c);
+            }
+            CardGame.instance.get_triggers().register(Phases.END_FILTER, this);
         }
+        
+        
+        @Override
+        public void execute() { //resetta il valore toughness delle creature ancora vive al valore iniziale
+            for (Creature c: CardGame.instance.get_current_player().get_creatures()){
+                c.setCurrent_toughness(c.get_toughness());
+            }
+            for (Creature c: CardGame.instance.get_current_adversary().get_creatures()){
+                c.setCurrent_toughness(c.get_toughness());
+            }
+        }
+        
     }
     public Effect get_effect(Player owner) { 
         return new Boiling_EarthEffect(owner, this);
