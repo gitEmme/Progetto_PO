1e34500c2a2cf69261217d98c7e5e68a1130f1ef
xenord <francesco.benetello921@gmail.com>
2016-04-10 12:39:25 +0200

    Aggiunto effetto savor the moment

--- CardGame/src/cardgame/cards/SavorTheMoment.java
+++ CardGame/src/cardgame/cards/SavorTheMoment.java
@@ -1,55 +1,50 @@
-
 package cardgame.cards;
 
 import cardgame.AbstractCardEffect;
 import cardgame.Card;
-import cardgame.CardGame;
 import cardgame.Effect;
 import cardgame.Phases;
 import cardgame.Player;
 import cardgame.SkipPhase;
-import cardgame.TriggerAction;
 
 
 public class SavorTheMoment implements Card{
     
-    public Effect get_effect(Player owner) { return new SavorTheMomentEffect(owner,this);}
-    
-    public String name() { return "Savor the moment";}
-    
-    public String type() { return "Sorcery";}
-    
-    public String rule_text() { return "The merrow took a moment for herself.\n She never gave it back";}
-    
-    public boolean isInstant() { return false;}
-    
-    public String toString() { return name() + "[" + rule_text() +"]";}
-    
-    private class SavorTheMomentEffect extends AbstractCardEffect implements TriggerAction
-    {
-        int i=0;
-    
-        public SavorTheMomentEffect(Player p,Card c) { super(p,c);}
-        public void resolve() {
+    private class SavorTheMomentEffect extends AbstractCardEffect {
         
-            owner.set_phase(Phases.UNTAP,new SkipPhase(Phases.UNTAP));
-            CardGame.instance.get_triggers().register(Phases.END_FILTER, this);
+        Player own;        
+        
+        public SavorTheMomentEffect(Player p, Card c) { 
+            super(p,c);
+            own = p;
             
-        } 
-
-        @Override
-        public void execute() {
-           
-            if(i>0)
-            {
-                
-                /*applicare il metodo next_Phase di DefaultPhaseManager e fare i--*/
-            }
-            
-            /*else if(i==0)
-            {
-                ripristinare il turno con set_phase di player
-            }*/    
         }
-    }    
+        @Override
+        public void resolve() {
+            
+            
+            SkipPhase skipNull = new SkipPhase(Phases.NULL);
+            SkipPhase skipUntap = new SkipPhase(Phases.UNTAP);
+            
+            own.set_phase(Phases.NULL, skipNull);
+            own.set_phase(Phases.UNTAP, skipUntap);
+        }
+    }
+
+    @Override
+    public Effect get_effect(Player owner) { 
+        return new SavorTheMomentEffect(owner, this); 
+    }
+    
+    @Override
+    public String name() { return "SavorTheMoment"; }
+    @Override
+    public String type() { return "Sorcery"; }
+    @Override
+    public String rule_text() { return name() + " Take an eztra turn after this one. Skip the untap step of that turn. "; }
+    @Override
+    public String toString() { return name() + "[" + rule_text() +"]";}
+    @Override
+    public boolean isInstant() { return false; }
+    
 }
