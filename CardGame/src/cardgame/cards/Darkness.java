257800ce169b99258dd9d272f0888bfe1c893f41
albysora <galvanitos@yahoo.it>
2016-04-10 23:30:28 +0200

    Update Darkness.java

--- CardGame/src/cardgame/cards/Darkness.java
+++ CardGame/src/cardgame/cards/Darkness.java
@@ -2,36 +2,48 @@ package cardgame.cards;
 
 import cardgame.AbstractCardEffect;
 import cardgame.Card;
-import cardgame.CardGame;
 import cardgame.Effect;
-import cardgame.Phases;
 import cardgame.Player;
-import cardgame.SkipPhase;
+import cardgame.CardGame;
+import cardgame.Creature;
+import cardgame.Phases;
+import cardgame.TriggerAction;
 
-
+// author Alberto
 public class Darkness implements Card{
     
-    private class DarknessEffect extends AbstractCardEffect {
-        
-        Player own;        
+    private class DarknessEffect extends AbstractCardEffect implements TriggerAction{
         
         public DarknessEffect(Player p, Card c) { 
             super(p,c);
-            own = p;
-            
         }
+        
         @Override
         public void resolve() {
-            Player target = CardGame.instance.get_current_player();
-            setTarget(target);
-            SkipPhase skipCombat = new SkipPhase(Phases.COMBAT);
-            getTarget().set_phase(Phases.COMBAT, skipCombat);
-
+            if(!CardGame.instance.get_current_player().get_creatures().isEmpty()){
+                for(int i=0; i<CardGame.instance.get_current_player().get_creatures().size();i++){
+                    CardGame.instance.get_current_player().get_creatures().get(i).setCurrent_power(0);
+                }
+            }
+            if(!CardGame.instance.get_current_adversary().get_creatures().isEmpty()){
+                for(int y=0; y<CardGame.instance.get_current_adversary().get_creatures().size(); y++){
+                    CardGame.instance.get_current_adversary().get_creatures().get(y).setCurrent_power(0);
+                }
+            }
         }
-
+        
         @Override
-        public void setTarget() {
-            throw new UnsupportedOperationException("Not supported yet.");
+        public void execute(){
+            if(!CardGame.instance.get_current_player().get_creatures().isEmpty()){
+                for(int i=0; i<CardGame.instance.get_current_player().get_creatures().size();i++){
+                    CardGame.instance.get_current_player().get_creatures().get(i).setCurrent_power(CardGame.instance.get_current_player().get_creatures().get(i).get_power());
+                }
+            }
+            if(!CardGame.instance.get_current_adversary().get_creatures().isEmpty()){
+                for(int y=0; y<CardGame.instance.get_current_adversary().get_creatures().size(); y++){
+                    CardGame.instance.get_current_adversary().get_creatures().get(y).setCurrent_power(CardGame.instance.get_current_player().get_creatures().get(y).get_power());
+                }
+            }
         }
         
     }
