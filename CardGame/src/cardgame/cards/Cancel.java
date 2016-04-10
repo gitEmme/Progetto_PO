428d9001be48b1f4ce5353f1365bdcaed0fa3112
xenord <francesco.benetello921@gmail.com>
2016-04-10 12:39:41 +0200

    Aggiunto effetto Cancel

--- CardGame/src/cardgame/cards/Cancel.java
+++ CardGame/src/cardgame/cards/Cancel.java
@@ -1,5 +1,7 @@
 /*
- * @Author Alessandro Zanolla
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package cardgame.cards;
 
@@ -7,24 +9,59 @@ import cardgame.AbstractCardEffect;
 import cardgame.Card;
 import cardgame.Effect;
 import cardgame.Player;
+import cardgame.CardGame;
 
-
+/**
+ *
+ * @author Luca
+ */
 public class Cancel implements Card{
-    
-    private class CancelEffect extends AbstractCardEffect {
-        public CancelEffect(Player p, Card c) { super(p,c); }
-        public void resolve() {}
+
+    private class CancelEffect extends AbstractCardEffect{
+
+        private CancelEffect(Player owner, Card card){
+            super(owner, card);
+        }
+        
+        @Override
+        public void resolve() {
+            if(CardGame.instance.get_stack().isEmpty()){
+                System.out.println("There no effects to cancel");
+            }else{
+                CardGame.instance.get_stack().removeLast();
+            }
+            
+        }
+        
+    }
+    @Override
+    public Effect get_effect(Player owner) {
+        return new CancelEffect(owner, this);
     }
 
-    public Effect get_effect(Player owner) { 
-        return new CancelEffect(owner, this); 
+    @Override
+    public String name() {
+        return "Cancel";
+    }
+
+    @Override
+    public String type() {
+        return "Instant";
+    }
+
+    @Override
+    public String rule_text() {
+        return "Counter target spell";
     }
     
-    public String name() { return "Cancel"; }
-    public String type() { return "Instant"; }
-    public String rule_text() { return "Counter target spell."; }
-    public String toString() { return "Cancel [Counter target spell]";}
-    public boolean isInstant() { return true; }
-    
+    @Override
+    public String toString(){
+        return name() + "[" + rule_text() + "]";
+    }
+
+    @Override
+    public boolean isInstant() {
+        return true;
+    }
     
 }
