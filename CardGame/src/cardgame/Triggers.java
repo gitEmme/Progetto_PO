c4571211186dee167733df018fef2673bbf26bed
xenord <francesco.benetello921@gmail.com>
2016-04-10 16:09:54 +0200

    Identazione

--- CardGame/src/cardgame/Triggers.java
+++ CardGame/src/cardgame/Triggers.java
@@ -7,12 +7,13 @@ public class Triggers {
     private class Pair { 
         public int filter; 
         public TriggerAction action;
-        public Pair(int f, TriggerAction a) { filter=f; action=a; }
+        public Pair(int f, TriggerAction a) { 
+            filter=f; action=a; 
+        }
         
     }
     
     ArrayList<Triggers.Pair> actions = new ArrayList<Triggers.Pair>();
-    
     
     public void register(int phase_trigger, TriggerAction a) {
         actions.add(new Triggers.Pair(phase_trigger, a));
